9fbaf278839842f4c0833b77c4634d7dc3aefb00
Ramesh L. Patel <patel.ramesh541@gmail.com>
2020-03-08 20:12:41 +0530

    adding gateway services for spring cloud

---
+++ hello-service/src/test/java/io/rock/helloservice/HelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rock.helloservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
