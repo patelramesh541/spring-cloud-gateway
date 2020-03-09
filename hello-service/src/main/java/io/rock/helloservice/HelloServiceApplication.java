3041ea7364518ed62ceb835269a094ef9f363a14
Ramesh L. Patel <patel.ramesh541@gmail.com>
2020-03-09 10:36:30 +0530

    adding gateway services for spring cloud

--- hello-service/src/main/java/io/rock/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/io/rock/helloservice/HelloServiceApplication.java
@@ -3,6 +3,7 @@ package io.rock.helloservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,8 @@ public class HelloServiceApplication {
 	}
 
 	@RequestMapping
-	public String hello()
+	public String hello(@RequestHeader("x-location") String location)
 	{
-		return "Hello";
+		return "Hello from " + location + " !";
 	}
 }
